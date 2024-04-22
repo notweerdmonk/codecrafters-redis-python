727ad4906e0ea58e59615dfc9ee6ac491fa98f53
notweerdmonk <wrdmnk@gmail.com>
2024-04-22 20:13:54 +0530

    2nd stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
 # Uncomment this to pass the first stage
 import socket
 
+class redis_parser(object):
+    @classmethod
+    def parse(self, data):
+        if len(data) == 0:
+            return
+
+        lines = data.splitlines()
+        startline = lines[0]
+        lines.pop(0)
+
+        iscommand = True if startline[0] == '*' else False
+
+        nparams = nparams = int(startline[1:]) if iscommand else 1
+
+        params = []
+        for i in range(nparams):
+            if lines[i][0] != '$':
+                raise RuntimeError('Invalid data')
+
+            nbytes = int(lines[i][1:])
+            param = lines[i+1][:nbytes]
+
+            params.append(param)
+
+        return params
+
+    def __init__(self, data):
+        self.params = self.parse(data)
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,7 +37,23 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, addr = server_socket.accept() # wait for client
+
+    print('Incoming connection from', addr)
+
+    with client_socket:
+        while True:
+            request_bytes = client_socket.recv(4096)
+            if not request_bytes:
+                break
+
+            request = request_bytes.decode('utf-8')
+
+            # Parser redis packet
+            command, *args = redis_parser.parse(request)
+
+            if command.upper() == 'PING':
+                client_socket.sendall(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
