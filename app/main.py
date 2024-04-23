ff3a6edf15c0312efcfcfbdf7a528e9511020ace
notweerdmonk <wrdmnk@gmail.com>
2024-04-23 14:35:39 +0530

    7th stage

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 # Uncomment this to pass the first stage
 import sys
 import socket
-from threading import Thread
+from threading import Thread, Lock
 from typing import Union
 from enum import Enum
+import time
+import random
+from dataclasses import dataclass
+
+def millis():
+    return time.time() * 1000
 
 class RESP_parser(object):
     @classmethod
@@ -70,6 +76,8 @@ class RESP_parser(object):
 class RESP_error(Enum):
     WRONG_ARGS = 1
     UNKNOWN_CMD = 2
+    SYNTAX = 3
+    CUSTOM = 4
 
 class RESP_builder(object):
 
@@ -102,7 +110,7 @@ class RESP_builder(object):
 
     @classmethod
     def error(
-        cls, command: str, args: list = None,
+        cls, command: str = '', args: list = None,
         typ: RESP_error = RESP_error.WRONG_ARGS
     ):
         if typ == RESP_error.WRONG_ARGS:
@@ -114,10 +122,121 @@ class RESP_builder(object):
             return f'-ERR unknown command `{command}`, '\
                    f'with args beginning with: {arg1}\r\n'.encode()
 
+        elif typ == RESP_error.SYNTAX:
+            return '-ERR syntax error\r\n'.encode()
+
+        elif typ == RESP_error.CUSTOM:
+            return f'-ERR {args}\r\n'.encode()
+
         else:
             raise RuntimeError('Unknown error type')
 
-store = {}
+@dataclass
+class StoreElement(object):
+    value: str
+    expiry : float
+
+class Store(object):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Store, cls).__new__(cls)
+            # Put any initialization here.
+        return cls._instance
+
+    def __init__(self):
+        self._lock: Lock = Lock()
+        self._store: dict[str, StoreElement] = {}
+
+    def expired(self, key):
+        with self._lock:
+            if key not in self._store:
+                return True
+    
+            e = self._store[key]
+            if e.expiry < 0:
+                return False
+            if e.expiry > int(time.time() * 1000):
+                return False
+
+            self._store.pop(key)
+            return True
+
+    def size(self):
+        with self._lock:
+            return len(self._store)
+
+    def keys(self):
+        with self._lock:
+            return self._store.keys()
+
+    # notice formatting
+    def set(self, key, value, expiry=-1):
+        with self._lock:
+            self._store[key] = StoreElement(value, (int(time.time() * 1000) + expiry) if expiry != -1 else expiry)
+
+    def get(self, key):
+        with self._lock:
+            if key not in self._store:
+                return ''
+            
+            e = self._store[key]
+            if e.expiry > 0 and e.expiry <= int(time.time() * 1000):
+                self._store.pop(key)
+                return ''
+            return e.value
+
+store = Store()
+
+def process_command(command, args):
+    response = None
+
+    if command.upper() == 'COMMAND':
+        response = RESP_builder.build(['ping', 'echo'])
+
+    elif command.upper() == 'PING':
+        response = RESP_builder.build('PONG', bulkstr=False)
+
+    elif command.upper() == 'ECHO':
+        if len(args) == 0:
+            return RESP_builder.error(command)
+
+        response = RESP_builder.build(args[0])
+
+    elif command.upper() == 'SET':
+        if len(args) < 2:
+            return RESP_builder.error(command)
+
+        exp = -1
+        if len(args) > 2:
+            expopt = args[2].upper()
+            if expopt == 'PX' and len(args) == 4:
+                exp = int(args[3])
+            else:
+                return RESP_builder.error(typ=RESP_error.SYNTAX)
+
+        store.set(args[0], args[1], exp)
+
+        response = RESP_builder.build('OK', bulkstr=False)
+
+    elif command.upper() == 'GET':
+        nargs = len(args)
+        if nargs < 1:
+            return RESP_builder.error(command)
+
+        if nargs == 1:
+            values = store.get(args[0])
+
+        else:
+            values = []
+            for i in range(nargs):
+                value = store.get(args[i])
+                values.append(value)
+
+        response = RESP_builder.build(values)
+
+    return response
 
 def handle_client(client_socket):
     with client_socket:
@@ -134,46 +253,17 @@ def handle_client(client_socket):
             response = None
 
             try:
-                if command.upper() == 'COMMAND':
-                    response = RESP_builder.build(['ping', 'echo'])
+                response = process_command(command, args)
 
-                elif command.upper() == 'PING':
-                    response = RESP_builder.build('PONG', bulkstr=False)
-
-                elif command.upper() == 'ECHO':
-                    if len(args) == 0:
-                        response =  RESP_builder.error(command)
-                    else:
-                        response = RESP_builder.build(args[0])
-
-                elif command.upper() == 'SET':
-                    if len(args) < 2:
-                        response =  RESP_builder.error(command)
-                    else:
-                        global store
-                        store[args[0]] = args[1]
-                        response = RESP_builder.build('OK', bulkstr=False)
-
-                elif command.upper() == 'GET':
-                    nargs = len(args)
-                    if nargs < 1:
-                        response =  RESP_builder.error(command)
-                    else:
-                        if nargs == 1:
-                            values = store.get(args[0], '')
-                        else:
-                            values = []
-                            for i in range(nargs):
-                                value = store.get(args[i], '')
-                                values.append(value)
-
-                        response = RESP_builder.build(values)
+            except ValueError as v:
+                sys.stderr.write(f'ValueError: {v}\n')
+                response = RESP_builder.error(args='value is not an integer or out of range', typ=RESP_error.CUSTOM) 
 
             except Exception as e:
                 sys.stderr.write(f'Exception occurred: {e}\n')
 
             finally:
-                # Error
+                # Default response
                 if response is None or response == '':
                     response =  RESP_builder.error(
                         command, args, typ=RESP_error.UNKNOWN_CMD
@@ -181,7 +271,27 @@ def handle_client(client_socket):
 
             client_socket.sendall(response)
 
+def check_expiry():
+    CHECK_INTERVAL = 30 # seconds
+
+    while True:
+        MAX_LOT_SIZE = 20
+
+        store_keys = store.keys()
+        store_size = len(store_keys)
+        if store_size:
+            lot_size = store_size if store_size < MAX_LOT_SIZE else MAX_LOT_SIZE
+            selected = random.sample(store_keys, lot_size)
+            for key in selected:
+                print(f'Checking {key} expiry: {store.expired(key)}')
+
+        time.sleep(CHECK_INTERVAL)
+
 def main():
+    # Start thread to check for key expiry
+    expiry_thread = Thread(target=check_expiry)
+    expiry_thread.start()
+
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
@@ -204,6 +314,8 @@ def main():
         num_conn += 1
 
     for t in client_threads: t.join()
+
+    expiry_thread.join()
 
 if __name__ == "__main__":
     main()
